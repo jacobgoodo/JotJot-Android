6f91fb83b4969de7f3e890eb3e8a637e6aa68704
Jacob <jacob.goodridge@hotmail.com>
2020-05-26 13:43:55 +1000

    Added new reminders activity, reminders_addButton function

--- app/src/main/java/fail/enormous/jotjot/RemindersActivity.kt
+++ app/src/main/java/fail/enormous/jotjot/RemindersActivity.kt
@@ -1,9 +1,11 @@
 package fail.enormous.jotjot
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class RemindersActivity : AppCompatActivity() {
 
@@ -12,5 +14,11 @@ class RemindersActivity : AppCompatActivity() {
         this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT // Force portrait mode
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reminders)
+
+        val reminders_addButton = findViewById<FloatingActionButton>(R.id.remindersAddButton) // Reminders Floating Action Button
+        reminders_addButton.setOnClickListener{
+            val showRemindersAddActivity = Intent(this, RemindersAddActivity::class.java) // show RemindersAddActivity declaration
+            startActivity(showRemindersAddActivity)
+        }
     }
 }
---
+++ app/src/main/java/fail/enormous/jotjot/RemindersAddActivity.kt
@@ -0,0 +1,12 @@
+package fail.enormous.jotjot
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RemindersAddActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reminders_add)
+    }
+}
