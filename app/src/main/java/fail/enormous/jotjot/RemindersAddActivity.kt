ccc5102abd3ba52c6be60459c92e0c7c320dc825
Jacob <jacob.goodridge@hotmail.com>
2020-05-26 19:03:49 +1000

    Force portrait mode when adding new reminders

--- app/src/main/java/fail/enormous/jotjot/RemindersAddActivity.kt
+++ app/src/main/java/fail/enormous/jotjot/RemindersAddActivity.kt
@@ -1,12 +1,16 @@
 package fail.enormous.jotjot
 
+import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class RemindersAddActivity : AppCompatActivity() {
 
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT // Force portrait mode
         setContentView(R.layout.activity_reminders_add)
     }
 }
