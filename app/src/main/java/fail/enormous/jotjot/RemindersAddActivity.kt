37b365827db553568e5d09d2082bc5bbe699734c
Jacob <jacob.goodridge@hotmail.com>
2020-05-26 21:04:42 +1000

    Date selection windows, buttons have unwritten functions

--- app/src/main/java/fail/enormous/jotjot/RemindersAddActivity.kt
+++ app/src/main/java/fail/enormous/jotjot/RemindersAddActivity.kt
@@ -3,9 +3,11 @@ package fail.enormous.jotjot
 import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.app.Dialog
+import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.DatePicker
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.DialogFragment
@@ -18,9 +20,18 @@ class RemindersAddActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT // Force portrait mode
         setContentView(R.layout.activity_reminders_add)
+
+        val remCreateButton = findViewById<Button>(R.id.createButton) // Find createButton
+
+        // ADD FUNCTIONS FOR SAVING DATA !!!
+
+        remCreateButton.setOnClickListener {
+            val showRemindersActivity = Intent(this, RemindersActivity::class.java)
+            startActivity(showRemindersActivity) // Show reminders page
+        }
     }
 
-    fun showDatePickerDialog(v: View) {
+    fun showDatePickerDialog(view: View) {
         val newFragment = DatePickerFragment()
         newFragment.show(supportFragmentManager, "datePicker")
     }
@@ -28,7 +39,7 @@ class RemindersAddActivity : AppCompatActivity() {
 
 class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
 
-    override fun onCreateDialog( savedInstanceState: Bundle?): Dialog {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         // Use the current date as the default date in the picker
         val c = Calendar.getInstance()
         val year = c.get(Calendar.YEAR)
@@ -41,5 +52,6 @@ class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener 
 
     override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
         // Do something with the date chosen by the user
+        // ADD FUNCTION TO ADD DATE TO DATABASE AND DISPLAY IT
     }
 }
