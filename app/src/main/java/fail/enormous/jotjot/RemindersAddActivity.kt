fc5f50dce7ae5f7fccf748d5fae29ffa9237dbd8
Jacob <jacob.goodridge@hotmail.com>
2020-05-26 19:44:19 +1000

    Frame for date selection

--- app/src/main/java/fail/enormous/jotjot/RemindersAddActivity.kt
+++ app/src/main/java/fail/enormous/jotjot/RemindersAddActivity.kt
@@ -1,9 +1,15 @@
 package fail.enormous.jotjot
 
 import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.app.Dialog
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.View
+import android.widget.DatePicker
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+import java.util.*
 
 class RemindersAddActivity : AppCompatActivity() {
 
@@ -12,5 +18,28 @@ class RemindersAddActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT // Force portrait mode
         setContentView(R.layout.activity_reminders_add)
+    }
+
+    fun showDatePickerDialog(v: View) {
+        val newFragment = DatePickerFragment()
+        newFragment.show(supportFragmentManager, "datePicker")
+    }
+}
+
+class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    override fun onCreateDialog( savedInstanceState: Bundle?): Dialog {
+        // Use the current date as the default date in the picker
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        // Create a new instance of DatePickerDialog and return it
+        return DatePickerDialog(activity, this, year, month, day)
+    }
+
+    override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
+        // Do something with the date chosen by the user
     }
 }
